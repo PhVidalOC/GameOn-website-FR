3463fb3493764fa47562f51b9fed684b6cdb4659
PhVidalOC <philippe.vidal@expleogroup.com>
2021-07-23 08:12:42 +0200

    Validation & confirmation

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -24,12 +24,13 @@ function launchModal() {
 /**
  * Ajout de la fonctionnalitée pour fermer la modale en cliquantsur la "X".
  */
-let span = document.getElementsByClassName('close')[0];
-span.addEventListener('click', closeModal);
-  function closeModal(e) {
-    modalbg.style.display = "none";
+let span = document.getElementsByClassName("close")[0];
+span.addEventListener("click", closeModal);
+function closeModal(e) {
+  modalbg.style.display = "none";
+  document.getElementById('form').style.display = 'block';
+  document.getElementById('succes').style.display = 'none';
 }
-
 
 // Formulaire 2
 /**
@@ -39,12 +40,16 @@ document.getElementById("form").addEventListener("submit", validate);
 function validate(event) {
   event.preventDefault();
 
+  let hasError = false;
+
+
   /**
-   * Controle du nombre de caractère du nom.
+   * Controle du nombre de lettres du nom.
    */
-  const first = document.getElementById("first");
+   const first = document.getElementById("first");
   if (first.value.length < 2) {
-    showError(first, 'le prénom doit avoir 2 lettres minimum');
+    showError(first, "le prénom doit avoir 2 lettres minimum");
+    hasError = true;
   } else {
     hideError(first);
   }
@@ -54,7 +59,8 @@ function validate(event) {
    */
   const last = document.getElementById("last");
   if (last.value.length < 2) {
-    showError(last, 'le nom doit avoir 2 lettres minimum');
+    showError(last, "le nom doit avoir 2 lettres minimum");
+    hasError = true;
   } else {
     hideError(last);
   }
@@ -63,73 +69,95 @@ function validate(event) {
    * Controle de validité de l'écriture de l'adresse mail.
    */
   const email = document.getElementById("email");
-  let emailRegExp = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
+  let emailRegExp = new RegExp(
+    "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$",
+    "g"
+  );
   let testEmail = emailRegExp.test(email.value);
   if (testEmail) {
     hideError(email);
   } else {
-    showError(email, 'adresse de messagerie non correcte');
+    showError(email, "adresse de messagerie non correcte");
+    hasError = true;
   }
-  
-/**
- * Controle de la saisie de la date
- */
+
+  /**
+   * Controle de la saisie de la date
+   */
   const birthdate = document.getElementById("birthdate");
   if (isNaN(birthdate.value)) {
     hideError(birthdate);
   } else {
-    showError(birthdate, 'Modifier la date');
+    showError(birthdate, "Modifiez la date");
+    hasError = true;
   }
 
   /**
    * Controle du nombre de participation au GameOn.
    */
   const games = document.getElementById("quantity");
-  if (isNaN(games.value) || (games.value < 0) || (games.value > 99) || (games.value.length===0)) {
-  showError(games, 'Le nombre doit être compris entre 0 et 99');
+  if (
+    isNaN(games.value) ||
+    games.value < 0 ||
+    games.value > 99 ||
+    games.value.length === 0
+  ) {
+    showError(games, "Le nombre doit être compris entre 0 et 99");
+    hasError = true;
   } else {
     hideError(games);
   }
-
 
   /**
    * Controle, avec la fonction "checklocation", qu'une ville à été selcetionnée.
    */
   const locationError = document.getElementById("location-error");
   if (!checkLocation()) {
-  locationError.innerText= 'veuillez choisir une ville';
+    locationError.innerText = "veuillez choisir une ville";
+    hasError = true;
   } else {
-  locationError.innerText = '';
+    locationError.innerText = "";
   }
 
-
-
   /**
-   * Controle de la cas cochée pour les CGU
+   * Controle de la case cochée pour les CGU
    */
   const checkbox1Input = document.getElementById("checkbox1");
   const cguError = document.getElementById("cgu-error");
   if (!checkbox1Input.checked) {
-    cguError.innerText= 'veuillez lire et accépter les CGU';
-    } else {
-      cguError.innerText = '';
-    }
-
+    cguError.innerText = "veuillez lire et accepter les CGU";
+    hasError = true;
+  } else {
+    cguError.innerText = "";
+  }
 
 /**
- * Fonction de retour d'erreur
- * @param {*} input 
- * @param {*} errorMsg 
+ * Controle des erreurs du formulaire et validation.
  */
-  function showError(input, errorMsg){
-    input.parentElement.setAttribute("data-error-visible", "true");
-    input.parentElement.setAttribute("data-error", errorMsg)
+  if (!hasError) {
+    document.getElementById('form').style.display = 'none';
+    document.getElementById('form').reset();
+    document.getElementById('succes').style.display = 'block';
   }
+}
 
-  function hideError(input){
-    input.parentElement.setAttribute("data-error-visible", "false");
-    input.parentElement.setAttribute("data-error", "")
-  }
+/**
+ * Fonction de retour d'erreur avec message
+ * @param {*} input
+ * @param {*} errorMsg
+ */
+function showError(input, errorMsg) {
+  input.parentElement.setAttribute("data-error-visible", "true");
+  input.parentElement.setAttribute("data-error", errorMsg);
+}
+
+/**
+ * Fonction de retour "pas d'erreur". Pas de message.
+ * @param {*} input 
+ */
+function hideError(input) {
+  input.parentElement.setAttribute("data-error-visible", "false");
+  input.parentElement.setAttribute("data-error", "");
 }
 
 /**
@@ -137,12 +165,12 @@ function validate(event) {
  * @returns boolean
  */
 function checkLocation() {
-    const locations = document.getElementsByName("location");
-    let res = false;
-    locations.forEach(l => {
-      if (l.checked){
-        res=true
-      }
-    })
-    return res;
+  const locations = document.getElementsByName("location");
+  let res = false;
+  locations.forEach((l) => {
+    if (l.checked) {
+      res = true;
+    }
+  });
+  return res;
 }
